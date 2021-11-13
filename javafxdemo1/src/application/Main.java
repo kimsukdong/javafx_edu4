456a1d7927e2aaedef39aa384015bf2221ff5956
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-11-13 21:32:48 +0900

    javafx demo

---
+++ javafxdemo1/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			Parent root =FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,600,400);
+			Image image = new Image("http://www.hoseo.ac.kr/resources/images/korean/sub/info/info04tab_sym_02.png");
+			primaryStage.getIcons().add(image);
+			primaryStage.setTitle("Sample");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ javafxdemo1/src/module-info.java
@@ -0,0 +1,8 @@
+module javafxdemo1 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.desktop;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
